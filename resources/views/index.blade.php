cdc1d3f963c9f9502be180dd35539295601e4830
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-11-02 16:58:28 +0400

    style: design single idea page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,27 +40,19 @@
             class="w-20 px-4 py-3 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 hover:border-gray-400 text-xxs rounded-xl">Vote</button>
         </div>
       </div>
-      <div class="flex px-2 py-6">
-        <a href="#" class="flex-none">
-          <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar"
-            class="w-14 h-14 rounded-xl">
-        </a>
-        <div class="mx-4">
+      <div class="flex flex-1 px-2 py-6">
+        <div class="flex-none">
+          <a href="#">
+            <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar"
+              class="w-14 h-14 rounded-xl">
+          </a>
+        </div>
+        <div class="w-full mx-4">
           <h4 class="text-xl font-semibold">
             <a href="#" class="hover:underline">A random title can go here</a>
           </h4>
           <div class="mt-3 text-gray-600 line-clamp-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum possimus quasi
-            reprehenderit
-            earum incidunt. Ad reprehenderit repudiandae dolorem ducimus modi accusamus beatae
-            perferendis? Eum eligendi nulla aliquam numquam! Nobis, tempore rerum autem accusamus
-            perspiciatis laudantium at excepturi sint placeat nulla beatae qui aliquam quibusdam
-            corrupti vel dolor praesentium quam dolore itaque deleniti quod in ut earum. Cum fuga ea
-            molestias nesciunt soluta sint maxime, magnam dolorum reprehenderit autem, vel ipsa?
-            Vitae
-            vel quos quaerat, fugit in reiciendis alias perspiciatis esse numquam facere cum veniam
-            beatae voluptatem eos amet vero laborum error. Dolor distinctio temporibus iure,
-            repellendus porro molestiae deleniti quibusdam?
+            Lorem ipsum dolor sit amet
           </div>
 
           <div class="flex items-center justify-between mt-6">
@@ -76,7 +68,7 @@
                 class="px-4 py-2 font-bold leading-none text-center uppercase bg-gray-200 rounded-full text-xxs w-28 h-7">
                 Open</div>
               <button
-                class="relative px-3 py-2 transition duration-150 ease-in bg-gray-100 rounded-full hover:bg-gray-200 h-7">
+                class="relative px-3 py-2 transition duration-150 ease-in bg-gray-100 border rounded-full hover:bg-gray-200 h-7">
                 <svg fill="currentColor" width="24" height="6">
                   <path
                     d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
