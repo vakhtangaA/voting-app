d435979c85393f7eeb47b0e805b8153be870e1df
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-11-06 16:38:35 +0400

    feat: add Category model
    create Category model with it's controller, factory, migration.
    also write tests for categories

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -32,4 +32,9 @@ class Idea extends Model
             ]
         ];
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use Tests\TestCase;
 use App\Models\Idea;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,28 +12,57 @@ class ShowIdeasTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function single_idea_shows_correctly_on_the_show_page()
+    public function single_idea_shows_correctly_on_the_main_page()
     {
-        $idea = Idea::factory()->create([
+        $categoryOne = Category::factory()->create([
+            'name' =>'Category 1'
+        ]);
+        $categoryTwo = Category::factory()->create([
+            'name' =>'Category 2'
+        ]);
+
+
+        $ideaOne = Idea::factory()->create([
             'title' => 'My First Idea',
+            'category_id' => $categoryOne->id,
             'description' => 'My first description'
         ]);
 
-        $response = $this->get(route('idea.show', $idea));
+        $ideaTwo = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'category_id' => $categoryTwo->id,
+            'description' => 'My first description'
+        ]);
+
+        $response = $this->get(route('idea.index'));
 
         $response->assertSuccessful();
-        $response->assertSee($idea->title);
-        $response->assertSee($idea->description);
+        $response->assertSee($ideaOne->title);
+        $response->assertSee($ideaOne->description);
+        $response->assertSee($categoryOne->name);
+        $response->assertSee($ideaTwo->title);
+        $response->assertSee($ideaTwo->description);
+        $response->assertSee($categoryTwo->name);
     }
 
     /** @test */
     public function same_idea_title_different_slugs()
     {
+        $categoryOne = Category::factory()->create([
+            'name' =>'Category 1'
+        ]);
+        $categoryTwo = Category::factory()->create([
+            'name' =>'Category 2'
+        ]);
+        
         $ideaOne = Idea::factory()->create([
             'title' => 'My First Idea',
+            'category_id' => $categoryOne->id,
+
         ]);
         $ideaTwo = Idea::factory()->create([
             'title' => 'My First Idea',
+            'category_id' => $categoryTwo->id,
         ]);
 
         $response = $this->get(route('idea.show', $ideaOne));
