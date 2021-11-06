bbbd16842440f5b34209364c0453cbc65031d7e4
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-11-06 12:22:41 +0400

    refactor: make whole idea component clickable

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -4,7 +4,8 @@
       <select name="category" id="category" class="w-full px-4 py-2 border-none rounded-xl">
         <option value="Cateogory One">Category One</option>
         <option value="Cateogory Two">Category Two</option>
-        <option value="Cateogory Three">Category Three</option>
+        <option value="Cateogory Three">Category Three
+        </option>
       </select>
     </div>
     <div class="w-full md:w-1/3">
@@ -12,7 +13,8 @@
         class="w-full px-4 py-2 border-none rounded-xl">
         <option value="Cateogory One">Category One</option>
         <option value="Cateogory Two">Category Two</option>
-        <option value="Cateogory Three">Category Three</option>
+        <option value="Cateogory Three">Category Three
+        </option>
       </select>
     </div>
     <div class="relative w-full md:w-2/3">
@@ -28,7 +30,16 @@
   </div> <!-- end filters -->
   <div class="my-6 space-y-6 ideas-container">
     @foreach ($ideas as $idea)
-    <div
+    <div x-data @click="
+        const clicked = $event.target;
+        const target = clicked.tagName.toLowerCase()
+
+        const ignores = ['button', 'svg', 'path', 'a']
+
+        if (!ignores.includes(target)) {
+          clicked.closest('.idea-container').querySelector('.idea-link').click()
+        }
+        "
       class="flex transition duration-150 ease-in bg-white cursor-pointer idea-container hover:shadow-card rounded-xl">
       <div class="hidden px-5 py-8 border-r border-gray-100 md:block">
         <div class="text-center">
@@ -50,7 +61,8 @@
         </div>
         <div class="flex flex-col justify-between w-full mx-4">
           <h4 class="text-xl font-semibold">
-            <a href="{{ route('idea.show', $idea) }}" class="hover:underline">{{ $idea->title }}</a>
+            <a href="{{ route('idea.show', $idea) }}"
+              class="hover:underline idea-link">{{ $idea->title }}</a>
           </h4>
           <div class="mt-3 text-gray-600 line-clamp-3">
             {{ $idea->description }}
@@ -58,11 +70,14 @@
 
           <div class="flex flex-col justify-between mt-6 md:items-center md:flex-row">
             <div class="flex items-center space-x-2 text-xs font-semibold text-gray-400">
-              <div>{{ $idea->created_at->diffForHumans() }}</div>
+              <div>
+                {{ $idea->created_at->diffForHumans() }}
+              </div>
               <div>&bull;</div>
               <div>Category 1</div>
               <div>&bull;</div>
-              <div class="text-gray-900">3 Comments</div>
+              <div class="text-gray-900">3 Comments
+              </div>
             </div>
             <div class="flex items-center mt-2 space-x-2 md:mt-0" x-data="{ isOpen: false }">
               <div
@@ -89,8 +104,10 @@
             </div>
             <div class="flex items-center mt-4 md:hidden md:mt-0">
               <div class="h-10 px-4 py-2 pr-8 text-center bg-gray-100 rounded-xl">
-                <div class="text-sm font-bold leading-none">12</div>
-                <div class="font-semibold leading-none text-gray-400 text-xxs">Votes</div>
+                <div class="text-sm font-bold leading-none">
+                  12</div>
+                <div class="font-semibold leading-none text-gray-400 text-xxs">
+                  Votes</div>
               </div>
               <button
                 class="w-20 px-4 py-3 -mx-5 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 text-xxs rounded-xl hover:border-gray-400">
