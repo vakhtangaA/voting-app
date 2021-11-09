3d4f9047805f231dfe601e89b5659ea6bd813d25
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-11-09 23:44:51 +0400

    chore: cs-fixer change indentation

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,64 +9,67 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+	use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var string[]
+	 */
+	protected $fillable = [
+		'name',
+		'email',
+		'password',
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
 
-    public function ideas()
-    {
-        return $this->hasMany(Idea::class);
-    }
+	public function ideas()
+	{
+		return $this->hasMany(Idea::class);
+	}
 
-    public function votes()
-    {
-        return $this->belongsToMany(Idea::class, 'votes');
-    }
+	public function votes()
+	{
+		return $this->belongsToMany(Idea::class, 'votes');
+	}
 
-    public function getAvatar()
-    {
-        $firstCharacter = $this->email[0];
+	public function getAvatar()
+	{
+		$firstCharacter = $this->email[0];
 
-        if (is_numeric($firstCharacter)) {
-            $intergerToUse = ord(strtolower($firstCharacter)) - 21;
-        } else {
-            $intergerToUse = ord(strtolower($firstCharacter)) - 96;
-        }
-  
-        return "https://en.gravatar.com/avatar/"
-         . md5($this->email)
-         . '?s=200'
-         .'&d=mp'
-         .'https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
-         .$intergerToUse
-         .'.png';
-    }
+		if (is_numeric($firstCharacter))
+		{
+			$intergerToUse = ord(strtolower($firstCharacter)) - 21;
+		}
+		else
+		{
+			$intergerToUse = ord(strtolower($firstCharacter)) - 96;
+		}
+
+		return 'https://en.gravatar.com/avatar/'
+		 . md5($this->email)
+		 . '?s=200'
+		 . '&d=mp'
+		 . 'https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
+		 . $intergerToUse
+		 . '.png';
+	}
 }
