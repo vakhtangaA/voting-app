00ea16bfd38a803cd1bbaedcf4da518dcea57384
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-11-12 18:00:15 +0400

    test: category filters tests

--- tests/Feature/VoteIndexPageTest.php
+++ tests/Feature/VoteIndexPageTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\Idea;
 use App\Models\User;
-use App\Models\Vote;
 use App\Models\Status;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -32,42 +31,6 @@ class VoteIndexPageTest extends TestCase
 		]);
 
 		$this->get(route('idea.index'))
-			->assertSeeLivewire('idea-index');
-	}
-
-	/** @test */
-	public function index_page_correctly_receives_votes_count()
-	{
-		$user = User::factory()->create();
-		$userB = User::factory()->create();
-
-		$categoryOne = Category::factory()->create(['name' => 'Category 1']);
-
-		$statusOpen = Status::factory()->create(['name'=> 'Open', 'classes' => 'bg-gray-200']);
-
-		$idea = Idea::factory()->create([
-			'user_id'     => $user->id,
-			'category_id' => $categoryOne->id,
-			'status_id'   => $statusOpen->id,
-			'title'       => 'My First Idea',
-			'description' => 'Description for my first idea',
-		]);
-
-		Vote::factory()->create([
-			'idea_id' => $idea->id,
-			'user_id' => $user->id,
-		]);
-
-		Vote::factory()->create([
-			'idea_id' => $idea->id,
-			'user_id' => $userB->id,
-		]);
-
-		// dd($idea);
-		$this->get(route('idea.index'))
-		->assertViewHas('ideas', function ($ideas) {
-			dd($ideas);
-			return $ideas->first()->votes_count == 2;
-		});
+			 ->assertSeeLivewire('idea-index');
 	}
 }
