c35675a742fa1d155cf41981cadd3b649604e7e2
vakhtangaa <vakhtang.chitauri@gmail.com>
2021-11-16 17:44:50 +0400

    refactor: new idea has your vote when created

--- app/Http/Livewire/CreateIdea.php
+++ app/Http/Livewire/CreateIdea.php
@@ -4,47 +4,52 @@ namespace App\Http\Livewire;
 
 use App\Models\Category;
 use App\Models\Idea;
-use Illuminate\Http\Response;
 use Livewire\Component;
 
 class CreateIdea extends Component
 {
-    public $title;
-    public $category = 1;
-    public $description;
+	public $title;
 
-    protected $rules = [
-        'title' => 'required|min:4',
-        'category' => 'required|integer',
-        'description' => 'required|min:4',
-    ];
+	public $category = 1;
 
-    public function createIdea()
-    {
-        if (auth()->check()) {
-            $this->validate();
-            
-            Idea::create([
-            'user_id' => auth()->id(),
-            'category_id' => $this->category,
-            'status_id' => 1,
-            'title' => $this->title,
-            'description' => $this->description
-        ]);
-            session()->flash('success_message', 'Idea was added successfully');
+	public $description;
 
-            $this->reset();
+	protected $rules = [
+		'title'       => 'required|min:4',
+		'category'    => 'required|integer',
+		'description' => 'required|min:4',
+	];
 
-            return redirect()->route('idea.index');
-        }
+	public function createIdea()
+	{
+		if (auth()->guest())
+		{
+			abort(Response::HTTP_FORBIDDEN);
+		}
 
-        abort(Response::HTTP_FORBIDDEN);
-    }
+		$this->validate();
 
-    public function render()
-    {
-        return view('livewire.create-idea', [
-            'categories' => Category::all()
-        ]);
-    }
+		$idea = Idea::create([
+			'user_id'     => auth()->id(),
+			'category_id' => $this->category,
+			'status_id'   => 1,
+			'title'       => $this->title,
+			'description' => $this->description,
+		]);
+
+		$idea->vote(auth()->user());
+
+		session()->flash('success_message', 'Idea was added successfully');
+
+		$this->reset();
+
+		return redirect()->route('idea.index');
+	}
+
+	public function render()
+	{
+		return view('livewire.create-idea', [
+			'categories' => Category::all(),
+		]);
+	}
 }
